fa7a1aa4ff8653fbfb4e14b0ec0aa922968d18b0
shinji <rill0131@gmail.com>
2017-09-19 02:42:09 +0900

    add name validation test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = User.new(name: "compass", email:"compass@example.com",
+  		password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "name should be present" do
+  	@user.name = " "
+  	assert_not @user.valid?
+  end
+
+  test "name validation should reject length over 50" do
+  	@user.name = "a" * 51
+  	assert_not @user.valid?
+  end
+
+
+
+
 end
