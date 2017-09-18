6e138b88472a6e62a5915023d7e0408b0aa5fb54
shinji <rill0131@gmail.com>
2017-09-19 02:09:58 +0900

    add validations for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   def new
-
   	@user = User.new
   end
 
   def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "登録が完了しました！"
+  		redirect_to root_url(@root)
+  	else
+  		render 'new'
+  	end
   end
 
   private
