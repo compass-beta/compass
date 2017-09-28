19e0a0f15b9e320618bd12b53f9b90bb807eb526
shinji <rill0131@gmail.com>
2017-09-29 00:17:39 +0900

    add session test about edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  #デフォルトでは、コントローラ内の全てのアクションに適用される(onlyでアクションの適用範囲を指定できる)
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def new
   	@user = User.new
   end
@@ -19,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       # 懇親に成功した場合
       flash[:success] = "Profile updated"
@@ -38,5 +39,22 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    #beforeアクション
+
+    #ログイン済みかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+    end
+
+    #正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,19 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	# 永続的セッションを付与する
 	def remember(user)
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
   	end
 
+  	# 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  	def current_user?(user)
+  		user == current_user
+  	end
+
+  	# 記憶トークン (cookie) に対応するユーザーを返す
 	def current_user
 		if(user_id = session[:user_id])
 			@current_user ||= User.find_by(id: user_id)
@@ -39,4 +46,17 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
+
+	# 記憶したURL (もしくはデフォルト値) にリダイレクト
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	# アクセスしようとしたURLを覚えておく
+	def store_location
+		# request.original_urlでリクエスト先が取得できる
+		session[:forwarding_url] = request.original_url if request.get?
+	end
+
 end
