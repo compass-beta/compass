b9cb5f8cb9e082d8f72d9922d421f6c331aabe71
shinji <rill0131@gmail.com>
2017-09-29 16:57:35 +0900

    add image cache

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   	def user_params
-  		params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :avatar_cache)
   	end
 
     #beforeアクション
