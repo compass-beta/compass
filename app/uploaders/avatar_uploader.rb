e23388bb2fbfbbe8db62fec059204e4ada0837a8
shinji <rill0131@gmail.com>
2017-09-29 15:03:48 +0900

    fix image uploader settings

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -29,6 +29,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  # JPEG 形式に変換して保存
+  process convert: "jpg"
+
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
@@ -36,14 +39,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "user.jpg" if original_filename
+  end
 
 end
